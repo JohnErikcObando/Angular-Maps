6d9f4bce761b0b6dbb531b80a938cba9c95bee40
JohnErikcObando <Jeobando11@hotmail.com>
2021-03-27 11:07:19 -0500

    Implementar marcador y informacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { AgmCoreModule } from '@agm/core';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAqVxO_Tz7FH2oUHv1PkMaqsBqCtu2alBw'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/mapa/mapa.component.ts
@@ -0,0 +1,29 @@
+import { Component, NgZone, OnInit } from '@angular/core';
+import { Marcador } from '../../classes/marcador.class';
+
+
+@Component({
+  selector: 'app-mapa',
+  templateUrl: './mapa.component.html',
+  styleUrls: ['./mapa.component.css']
+})
+export class MapaComponent implements OnInit {
+
+  marcadores: Marcador[] = [];
+
+  lat = 51.678418;
+  lng = 7.809007;
+
+  constructor() {
+
+    const nuevoMarcador= new Marcador(51.678418,7.809007);
+    this.marcadores.push(nuevoMarcador);
+  }
+
+  ngOnInit() {
+  }
+
+  agregarMarcador(evento) {
+    console.log(evento)
+  }
+}
